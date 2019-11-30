e8f34c7e7e48c44abb94db059a40699f74cdb44c
guigui64 <guillaume.comte10@gmail.com>
2019-11-30 17:48:49 +0100

    zip js

--- js/pages/2016-12.js
+++
@@ -1,6 +0,0 @@
-export default () => (
-  <div class="container" style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}>
-    <h1 class="text-center">Nope</h1>
-    <p class="text-center">Sorry, not yet implemented... Come back later !</p>
-  </div>
-);
--- js/utilities/copy-nope.cmd
+++
@@ -1,1 +0,0 @@
-for year in 2017 2016 2015; do for ((day=1; day <= 25; day++)); do cp -vn utilities/nope.js pages/2015-26.js ; done ; done

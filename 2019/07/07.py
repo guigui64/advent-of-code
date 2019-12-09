f54afdabbc8d4311e9847127190d1496700de5da
Guillaume Comte <guillaume.comte@airbus.com>
2019-12-09 14:19:58 +0100

    2019/07 part2

--- 2019/07/07.py
+++ 2019/07/07.py
@@ -1,11 +1,11 @@
 import os
+import threading
 from itertools import permutations
+from queue import Queue
 
 
-def intcode(code, inputs):
-    outputs = []
+def intcode(name, code, in_queue, out_queue):
     idx = 0
-    inputIdx = 0
     while True:
         try:
             instr = f"{code[idx]:05}"
@@ -30,13 +30,13 @@ def intcode(code, inputs):
                 idx += length
             elif opcode == 3:  # Save input
                 length = 2
-                code[code[idx + 1]] = inputs[inputIdx]
-                inputIdx += 1
+                input = in_queue.get()
+                code[code[idx + 1]] = input
                 idx += length
             elif opcode == 4:  # Output
                 length = 2
                 params = get_params(length)
-                outputs.append(params[0])
+                out_queue.put(params[0])
                 idx += length
             elif opcode == 5:  # Jump if true
                 length = 3
@@ -69,12 +69,12 @@ def intcode(code, inputs):
                     code[code[idx + 3]] = 0
                 idx += length
             elif opcode == 99:  # Halt
-                return [code, outputs]
+                return code
             else:  # should not happen
-                print(f"Error, opcode {opcode} unknown at index {idx}")
+                print(f"{name} : Error, opcode {opcode} unknown at index {idx}")
                 return -1
         except IndexError:
-            print("Index out of range")
+            print(f"{name} : Index out of range")
             return -1
 
 
@@ -84,14 +84,40 @@ def part1(input):
     for phases in permutations(range(5), 5):
         signal = 0
         for i, amplifier in enumerate("ABCDE"):
-            res = intcode(software.copy(), [phases[i], signal])
-            signal = res[1][-1]
+            name = "amplifier-" + amplifier
+            in_queue = Queue()
+            in_queue.put(phases[i])
+            in_queue.put(signal)
+            out_queue = Queue()
+            intcode(name, software.copy(), in_queue, out_queue)
+            signal = out_queue.get()  # should be the only element
         highest_signal = max(highest_signal, signal)
     return highest_signal
 
 
 def part2(input):
-    pass
+    software = [int(i) for i in input[0].split(",")]
+    highest_signal = 0
+    for phases in permutations(range(5, 10), 5):
+        N = 5
+        queues = [Queue() for _ in range(N)]
+        for i in range(N):
+            queues[i].put(phases[i])
+        queues[0].put(0)
+        amplifiers = []
+        for i, letter in enumerate("ABDCE"):
+            name = "amplifier-" + letter
+            amplifier = threading.Thread(
+                name=name,
+                target=intcode,
+                args=(name, software.copy(), queues[i], queues[(i + 1) % N]),
+            )
+            amplifier.start()
+            amplifiers.append(amplifier)
+        for amp in amplifiers:
+            amp.join()
+        highest_signal = max(highest_signal, queues[0].get())
+    return highest_signal
 
 
 if __name__ == "__main__":

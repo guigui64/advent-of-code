2d8e42b2baaddf23466b9849639676c67219c787
guigui64 <guillaume.comte10@gmail.com>
2020-12-06 17:58:05 +0100

    2020/06

---
+++ 2020/06.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    groups = open("06.txt").read().split("\n\n")
+    print(sum([len(set("".join(g.split()))) for g in groups]))
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    print(
+        sum(
+            [
+                sum([all([letter in g for g in group.split()]) for letter in alphabet])
+                for group in groups
+            ]
+        )
+    )

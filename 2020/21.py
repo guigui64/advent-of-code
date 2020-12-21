06c85e914a26fd4f63d5a42b456aad86443e2d70
guigui64 <guillaume.comte10@gmail.com>
2020-12-21 20:47:17 +0100

    2020/21

--- 2020/21.py
+++ 2020/21.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 import re
-from collections import defaultdict, Counter
+from collections import Counter
 
 pattern = re.compile(r"(.*) \(contains (.*)\)")
 
 
 def solve(fname):
-    # allergens is a dict of lists of ingredients list per allergen
-    allergens = defaultdict(list)
+    # allergens is a dict of sets of ingredients per allergen
+    allergens = {}
     # ingredients is a Counter of the ingredients
     ingredients = Counter()
     with open(fname) as f:
@@ -17,39 +17,33 @@ def solve(fname):
             i_ingredients = i_ingredients.split()
             i_allergens = i_allergens.split(", ")
             for allergen in i_allergens:
-                allergens[allergen] += [i_ingredients]
+                if allergen in allergens:
+                    allergens[allergen] = allergens[allergen] & set(i_ingredients)
+                else:
+                    allergens[allergen] = set(i_ingredients)
             for ing in i_ingredients:
                 ingredients[ing] += 1
-    # filter allergens by intersecting their ingredients lists
-    allergens_filtered = {}
-    for allergen, ing_list in allergens.items():
-        filtered = set(ing_list[0])
-        for ings in ing_list[1:]:
-            filtered.intersection_update(set(ings))
-        allergens_filtered[allergen] = filtered
     # safe ingredients are ingredients that cannot have any of the allergens found
     safe_ingredients = set(ingredients)
-    for ing in allergens_filtered.values():
-        safe_ingredients.difference_update(ing)
+    for ing in allergens.values():
+        safe_ingredients -= ing
     # answer to part1 is the number of times all safe ingredients where found
     part1 = str(sum(ingredients[ing] for ing in safe_ingredients))
     # remove safe ingredients from allergens
-    for allergen, ing in allergens_filtered.items():
-        ing.difference_update(safe_ingredients)
+    for ing in allergens.values():
+        ing -= safe_ingredients
     # eliminate
     allergens_final = {}  # dict whose key is NOW the ingredient
-    while len(allergens := allergens_filtered.keys()) > 0:
+    while len(allergens) > 0:
         for allergen in allergens:
-            allergens_filtered[allergen].difference_update(allergens_final.keys())
-            if len(ing := allergens_filtered[allergen]) == 1:
+            allergens[allergen] -= set(allergens_final.keys())
+            if len(ing := allergens[allergen]) == 1:
                 allergens_final[list(ing)[0]] = allergen
-                del allergens_filtered[allergen]
+                del allergens[allergen]
                 break
     # answer to part 2 is the canonical dangerous ingredient list sorted alphabetically by their
     # allergen
-    part2 = ",".join(
-        ing for ing, _ in sorted(allergens_final.items(), key=lambda t: t[1])
-    )
+    part2 = ",".join(ing for ing in sorted(allergens_final, key=allergens_final.get))
     return part1, part2
 
 

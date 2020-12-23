8875b71edeee0538f3085ff0ce006d83146f7ee0
guigui64 <guillaume.comte10@gmail.com>
2020-12-23 15:38:09 +0100

    2020/22

--- 2020/22.py
+++ 2020/22.py
@@ -12,14 +12,40 @@ def parse(fname):
     return players_decks
 
 
-if __name__ == "__main__":
-    fname = "22ex.txt"
-    players_decks = parse(fname)
+def combat(players_decks, recursive=False):
+    old_states = []
     while all(len(deck) != 0 for deck in players_decks):
+        # Manage old states
+        if recursive and str(players_decks) in old_states:
+            return 0  # No infinite game => player 1 wins
+        old_states.append(str(players_decks))
+        # Draw cards
         top_cards = [deck.popleft() for deck in players_decks]
-        winner = max((v, i) for (i, v) in enumerate(top_cards))[1]
-        [players_decks[winner].append(f(top_cards)) for f in (max, min)]
-    deck = [deck for deck in players_decks if len(deck) > 0][0]
+        if recursive and all(len(players_decks[i]) >= top_cards[i] for i in range(2)):
+            # sub-game
+            new_decks = [
+                deque(list(players_decks[i])[: top_cards[i]]) for i in range(2)
+            ]
+            winner = combat(new_decks, True)
+        else:
+            winner = max((v, i) for (i, v) in enumerate(top_cards))[1]
+        players_decks[winner].append(top_cards.pop(winner))
+        players_decks[winner].append(top_cards.pop())
+    winner = [i for (i, deck) in enumerate(players_decks) if len(deck) != 0][0]
+    return winner
+
+
+if __name__ == "__main__":
+    fname = "22.txt"
+    # Part 1 : Combat
+    players_decks = parse(fname)
+    winner = combat(players_decks)
+    deck = players_decks[winner]
     deck.reverse()
     print("part1:", sum((i + 1) * v for (i, v) in enumerate(deck)))
+    # Part 2 : Recursive Combat
     players_decks = parse(fname)
+    winner = combat(players_decks, True)
+    deck = players_decks[winner]
+    deck.reverse()
+    print("part2:", sum((i + 1) * v for (i, v) in enumerate(deck)))

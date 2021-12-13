864029b45ee0b12141d5246453eec0689afad071
Guillaume Comte <guillaume.comte@airbus.com>
2021-12-13 12:23:37 +0100

    day 12

--- 2021/12.py
+++ 2021/12.py
@@ -10,7 +10,30 @@ def main():
         a, b = line.split("-")
         G[a] += [b]
         G[b] += [a]
-    # TODO traverse G
+
+    def traverse(cave, path, paths, part2=False, revisited=None):
+        for cave in G[cave]:
+            if cave == "end":
+                path.append(cave)
+                paths.append(path)
+            elif cave.islower() and cave in path:
+                if part2 and revisited is None and cave != "start":
+                    npath = path.copy()
+                    npath.append(cave)
+                    traverse(cave, npath, paths, True, cave)
+                else:
+                    pass
+            else:
+                npath = path.copy()
+                npath.append(cave)
+                traverse(cave, npath, paths, part2, revisited)
+
+    paths = []
+    traverse("start", ["start"], paths)
+    print(len(paths))
+    paths = []
+    traverse("start", ["start"], paths, True)
+    print(len(paths))
 
 
 __name__ == "__main__" and main()

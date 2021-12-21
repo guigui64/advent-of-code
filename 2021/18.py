6ec00ee8c8381e919bae7c72435a3eb1345f01b4
guigui64 <guillaume.comte10@gmail.com>
2021-12-21 12:13:38 +0100

    day 18

--- 2021/18.py
+++ 2021/18.py
@@ -1,34 +1,92 @@
 #!/usr/bin/env python
 
 import aoc
+from itertools import permutations
 
 
-def magnitude(pair):
-    l, r = pair
-    if type(l) != int:
-        l = magnitude(l)
-    if type(r) != int:
-        r = magnitude(r)
-    return 3 * l + 2 * r
+def reduce(n):
+    reduced = True
+    while reduced:
+        n, reduced, *_ = explode(n, 0)
+        if not reduced:
+            n, reduced = split(n)
+    return n
 
 
-def reduce(pair, depth=0):
-    l, r = pair
-    if depth == 3:
-        pass
-    return pair
+def explode(n, level):
+    if not isinstance(n, int):
+        l, r = n
+        if level >= 4:
+            return 0, True, l, r
+        else:
+            l, reduced, expl, expr = explode(l, level + 1)
+            if reduced:
+                if expr != 0:
+                    r = add_left(r, expr)
+                    expr = 0
+            else:
+                r, reduced, expl, expr = explode(r, level + 1)
+                if reduced:
+                    if expl != 0:
+                        l = add_right(l, expl)
+                        expl = 0
+            if reduced:
+                return (l, r), True, expl, expr
+    return n, False, 0, 0
+
+
+def add_left(n, m):
+    if isinstance(n, int):
+        return n + m
+    else:
+        a, b = n
+        return add_left(a, m), b
+
+
+def add_right(n, m):
+    if isinstance(n, int):
+        return n + m
+    else:
+        a, b = n
+        return a, add_right(b, m)
+
+
+def split(n):
+    if isinstance(n, int):
+        if n >= 10:
+            a = n // 2
+            return (a, n - a), True
+    else:
+        l, r = n
+        l, reduced = split(l)
+        if not reduced:
+            r, reduced = split(r)
+        if reduced:
+            return (l, r), True
+    return n, False
+
+
+def magnitude(n):
+    if isinstance(n, int):
+        return n
+    l, r = n
+    return 3 * magnitude(l) + 2 * magnitude(r)
 
 
 def main():
-    # for line in aoc.input():
-    #     print(line)
-    #     # TODO
-    print(magnitude([[1, 2], [[3, 4], 5]]))
-    print(magnitude([[[[0, 7], 4], [[7, 8], [6, 0]]], [8, 1]]))
-    print(magnitude([[[[1, 1], [2, 2]], [3, 3]], [4, 4]]))
-    print(magnitude([[[[3, 0], [5, 3]], [4, 4]], [5, 5]]))
-    print(magnitude([[[[5, 0], [7, 4]], [5, 5]], [6, 6]]))
-    print(magnitude([[[[8, 7], [7, 7]], [[8, 6], [7, 7]]], [[[0, 7], [6, 6]], [8, 7]]]))
+    sum = None
+    numbers = [eval(line) for line in aoc.input()]
+    for number in numbers:
+        if sum is None:
+            sum = number
+        else:
+            sum = reduce([sum, number])
+    print(magnitude(sum))
+    maxmag = 0
+    for (n1, n2) in permutations(numbers, 2):
+        mag = magnitude(reduce([n1, n2]))
+        maxmag = max(maxmag, mag)
+    print(maxmag)
 
 
 __name__ == "__main__" and main()

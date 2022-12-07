00669dd121493ee6ef199b0bbe3c3332f6649cff
guigui64 <guillaume.comte10@gmail.com>
2022-12-07 23:00:24 +0100

    2022/07 use class instead of simple type
    because why not

--- 2022/07.ts
+++ 2022/07.ts
@@ -1,35 +1,35 @@
 import { part1, part2, printTime, readLines, startTimer, sum } from "./aoc.ts";
 
-type File = {
+class File {
   parent?: File;
   children: File[];
   fullname: string;
   size: number;
-};
-function newFile(name: string, parent?: File, size = 0): File {
-  return {
-    parent,
-    children: [],
-    fullname: parent ? parent.fullname + "/" + name : name,
-    size,
-  };
-}
 
-const sizes: Map<string, number> = new Map();
-function size(file: File) {
-  if (file.children.length === 0) {
-    // this is not a dir: simply return the file size, no need to store in map
-    return file.size;
+  constructor(name: string, parent?: File, size = 0) {
+    this.parent = parent;
+    this.children = [];
+    this.fullname = parent ? parent.fullname + "/" + name : name;
+    this.size = size;
   }
-  if (!sizes.has(file.fullname)) {
-    sizes.set(file.fullname, sum(file.children.map(size)));
+
+  // store sizes per dir to get solutions of both parts + use as cache
+  static sizes: Map<string, number> = new Map();
+  getSize() {
+    if (this.children.length === 0) {
+      // this is not a dir: simply return the this size, no need to store in map
+      return this.size;
+    }
+    if (!File.sizes.has(this.fullname)) {
+      File.sizes.set(this.fullname, sum(this.children.map((f) => f.getSize())));
+    }
+    return File.sizes.get(this.fullname)!;
   }
-  return sizes.get(file.fullname)!;
 }
 
 startTimer();
 const lines = readLines(false);
-const root = newFile("/");
+const root = new File("/");
 let dir = root;
 for (const line of lines) {
   if (line.startsWith("$ cd /")) {
@@ -37,7 +37,7 @@ for (const line of lines) {
   } else if (line.startsWith("$ cd ..")) {
     dir = dir.parent || root; // fancy real FS emulation :)
   } else if (line.startsWith("$ cd")) {
-    const newDir = newFile(line.slice(5), dir);
+    const newDir = new File(line.slice(5), dir);
     dir.children.push(newDir);
     dir = newDir;
   } else if (line.startsWith("$ ls")) {
@@ -46,17 +46,17 @@ for (const line of lines) {
     continue;
   } else {
     const [size, name] = line.split(" ");
-    dir.children.push(newFile(name, dir, parseInt(size)));
+    dir.children.push(new File(name, dir, parseInt(size)));
   }
 }
-const total = size(root);
-part1(sum(Array.from(sizes.values()).filter((s) => s <= 100_000)));
+const total = root.getSize();
+part1(sum(Array.from(File.sizes.values()).filter((s) => s <= 100_000)));
 printTime();
 
 // part2
 part2(
   Math.min(
-    ...Array.from(sizes.values()).filter(
+    ...Array.from(File.sizes.values()).filter(
       (size) => size >= 30_000_000 - (70_000_000 - total),
     ),
   ),

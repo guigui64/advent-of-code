d8a60c7a3af35effefa543ed4bcbd4d159989833
guigui64 <guillaume.comte10@gmail.com>
2022-12-11 22:05:57 +0100

    2022/10+11

---
+++ 2022/10.ts
@@ -0,0 +1,57 @@
+import {
+  log,
+  part1,
+  part2,
+  printTime,
+  readLines,
+  setDebug,
+  startTimer,
+  sum,
+} from "./aoc.ts";
+
+startTimer();
+const example = true;
+setDebug(example);
+const lines = readLines(example);
+
+let X = 1;
+const history: number[] = [X];
+for (const line of lines) {
+  log(line);
+  if (line === "noop") {
+    history.push(X);
+    log(X);
+  } else {
+    history.push(X);
+    log(X);
+    history.push(X);
+    log(X);
+    X += parseInt(line.slice(5));
+  }
+}
+const indices = [20, 60, 100, 140, 180, 220];
+indices.forEach((i) => log(i, history[i]));
+part1(
+  sum(indices.map((x) => x * history[x])),
+);
+
+let screen = "\n";
+for (let y = 0; y < 6; y++) {
+  log("y", y);
+  for (let x = 0; x < 40; x++) {
+    log("x + 1", x + 1);
+    const X = history[40 * y + x];
+    log("X", X);
+    if (Math.abs(X - x + 1) < 2) {
+      screen += "#";
+    } else {
+      screen += ".";
+    }
+    log(screen[screen.length - 1]);
+  }
+  screen += "\n";
+}
+
+printTime();
+part2(screen);
+printTime();
---
+++ 2022/11.ts
@@ -0,0 +1,74 @@
+import {
+  log,
+  part1,
+  part2,
+  printTime,
+  range,
+  readLines,
+  setDebug,
+  startTimer,
+} from "./aoc.ts";
+
+class Monkey {
+  id: number;
+  items: number[];
+  operation: string;
+  diviser: number;
+  throwTrue: number;
+  throwFalse: number;
+  counter = 0;
+
+  constructor(input: string[]) {
+    this.id = parseInt(input[0].slice(7));
+    this.items = input[1].split(": ")[1].split(", ").map(Number);
+    this.operation = input[2].split("new = ")[1];
+    this.diviser = parseInt(input[3].split("by ")[1]);
+    this.throwTrue = parseInt(input[4].split("monkey ")[1]);
+    this.throwFalse = parseInt(input[5].split("monkey ")[1]);
+  }
+}
+
+startTimer();
+const example = false;
+setDebug(example);
+const lines = readLines(example);
+lines.push("");
+
+const nbMonkeys = lines.length / 7;
+for (const part of [1, 2]) {
+  const monkeys = [];
+  for (let i = 0; i < nbMonkeys; i++) {
+    monkeys.push(new Monkey(lines.slice(i * 7, i * 7 + 6)));
+  }
+
+  const allModulos = monkeys.map((m) => m.diviser).reduce((a, c) => a * c, 1);
+  for (const round of range(part === 1 ? 20 : 10000)) {
+    for (const monkey of monkeys) {
+      while (monkey.items.length > 0) {
+        const item = monkey.items.shift()!;
+        const oper = monkey.operation.replaceAll("old", item.toString());
+        let worryLvl: number = eval(oper);
+        if (part === 1) {
+          worryLvl = Math.floor(worryLvl / 3);
+        } else {
+          worryLvl %= allModulos;
+        }
+        const monkeyDst = monkeys[
+          (worryLvl % monkey.diviser === 0)
+            ? monkey.throwTrue
+            : monkey.throwFalse
+        ];
+        monkeyDst.items.push(worryLvl);
+        monkey.counter++;
+      }
+    }
+    if (round + 1 === 1 || round + 1 === 20 || (round + 1) % 1000 === 0) {
+      log(round + 1, monkeys.map((m) => m.counter));
+    }
+  }
+  const amounts = monkeys.map((m) => m.counter);
+  log(amounts);
+  amounts.sort((a, b) => b - a);
+  part1(amounts[0] * amounts[1]);
+  printTime();
+}
